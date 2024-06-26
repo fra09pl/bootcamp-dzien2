34a67d6b70610182fb37bd79bf7677fc4bb36aa6
fra09pl <Kimpel.franciszek@gmail.com>
2024-06-26 18:45:24 +0200

    blog create

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
+        <h2 class="text-blue-600">Wpisy na Bloga</h2>
         <button @click="pobierzWpisy">refresh</button>
-        siema blog
-        {{ wpisy }}
+        <div v-for="wpis in wpisy">
+            <p>{{ wpis }}</p>
+        </div>
         <input v-model="nowyBlog" type="text">
         <button @click="dodajWpisy">dodaj</button>
     </div>
@@ -26,5 +28,8 @@ export default {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
+    async mounted(){
+        this.pobierzWpisy()
+    }
 }
 </script>
