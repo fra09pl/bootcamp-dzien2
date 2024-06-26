f658ffb2dede4d58386e2ceefc7583219c31ecb3
fra09pl <Kimpel.franciszek@gmail.com>
2024-06-26 19:06:43 +0200

    continue blog

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h2 class="text-blue-600">Wpisy na Bloga</h2>
-        <button @click="pobierzWpisy">refresh</button>
-        <div v-for="wpis in wpisy">
-            <p>{{ wpis }}</p>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
         </div>
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-x1 bg-stone-300">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4">dodaj</button>
+        </div>
+   
     </div>
 </template>
 
